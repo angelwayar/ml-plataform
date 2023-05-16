e09e261e8530f49620a5980a78832d773e400519
amway <angelmanuelwayar@gmail.com>
2023-05-16 19:47:22 -0400

    add new package and create hash class

--- app/api/user/routes/user_routes.py
+++ app/api/user/routes/user_routes.py
@@ -3,11 +3,12 @@ import sys
 from fastapi import status, Depends
 
 sys.path.append("..")
+from app.core.utils.hashing import Hasher
 from app.api.user.routes import router
-from app.domain.user.entities.user_command import UserCommand
-from app.domain.user.entities.user_query import UserResult
-from app.domain.user.usecases.create_user_use_case import CreateUserUseCase
 from app.dependencies import get_create_user_use_case
+from app.domain.user.entities.user_query import UserResult
+from app.domain.user.entities.user_command import UserCommand
+from app.domain.user.usecases.create_user_use_case import CreateUserUseCase
 
 
 @router.post(
@@ -19,7 +20,10 @@ def create_user(
         data: UserCommand,
         create_user_use_case: CreateUserUseCase = Depends(get_create_user_use_case)
 ):
-    user = create_user_use_case((data,))
+    hash_pwd = Hasher.get_password_hash(password=data.password)
+    new_data = UserCommand(username=data.username, password=hash_pwd)
+    user = create_user_use_case((new_data,))
+
     return user
 
 
