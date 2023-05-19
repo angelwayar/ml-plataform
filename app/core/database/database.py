11df40273c09da96815f3b7d1e0c18f43739a352
amway <angelmanuelwayar@gmail.com>
2023-05-19 11:56:05 -0400

    change name data base

--- app/core/database/database.py
+++ app/core/database/database.py
@@ -1,13 +1,13 @@
 from typing import Iterator
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./boliche.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./ml_plataform.db'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={'check_same_thread': False},
-    future=True,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
